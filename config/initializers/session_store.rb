b3adacfbc8e521b031242df433be67d298df7941
Steve Jerman <stjerman@cisco.com>
2014-04-11 18:35:53 -0600

    Clear before resetting

--- config/initializers/session_store.rb
+++
@@ -1,8 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-KfTest::Application.config.session_store :cookie_store, key: '_kf-test_session'
-
-# Use the database for sessions instead of the cookie-based default,
-# which shouldn't be used to store highly confidential information
-# (create the session table with "rails generate session_migration")
-# KfTest::Application.config.session_store :active_record_store
